33a169a67763d11a3e370b55491ae958577b3c9f
iskandar <106257364+Iskandar-web-dev@users.noreply.github.com>
2023-04-29 13:41:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let audio = document.querySelector('audio')
+let heart = document.querySelectorAll('.wrapper')
+let cont  = document.querySelector('.container')
+let btn = document.querySelector('.click')
+let stop = document.querySelector('.stop')
+let isPlaying = false
+
+
+    btn.onclick = () => {
+        heart[2].style.marginLeft = '50%'
+        setTimeout(() => {
+            heart[1] .style.marginLeft = '50%'
+        }, 1000)
+        setTimeout(() => {
+            heart[0] .style.marginLeft = '50%'
+        }, 2000)
+        setTimeout(() => {
+            heart.forEach(el => {
+                el.style.marginLeft = '-20%'
+            })
+        }, 2550)
+
+        audio.play()
+    }
+
+stop.onclick = () => {
+            isPlaying = true
+            audio.pause()
+}
